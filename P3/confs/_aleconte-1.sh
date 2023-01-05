d744debfa777aad574f71ad131b16048b23be715
ldes-cou <ldes-cou@student.42.fr>
2023-01-05 12:57:20 +0100

    adding comments to spine script

--- P3/confs/_aleconte-1.sh
+++ P3/confs/_aleconte-1.sh
@@ -7,6 +7,7 @@ vtysh << EOF
 conf t
 no ipv6 forwarding
 !
+# Setup the spine/route reflector interfaces
 interface eth0
 ip address 10.1.1.1/30
 !
@@ -16,21 +17,28 @@ ip address 10.1.1.5/30
 interface eth2
 ip address 10.1.1.9/30
 !
+# Setup the loopback interface 
 interface lo
 ip address ${RR_IP}/32
 !
+
+# Setup BGP group for AS 1 with the name ibgp 
 router bgp 1
 neighbor ibgp peer-group
 neighbor ibgp remote-as 1
 neighbor ibgp update-source lo
 bgp listen range 1.1.1.0/29 peer-group ibgp
 !
+
+# Enable the L2VPN control plane adding all leafs as neighbor and add route-reflector client relation
 address-family l2vpn evpn
 neighbor ibgp activate
 neighbor ibgp route-reflector-client
 exit-address-family
 !
+
 router ospf
+# Add every addresses to area 0 (backbone)
 network 1.0.0.0/8 area 0
 network 10.0.0.0/8 area 0
 !
@@ -38,5 +46,7 @@ line vty
 !
 exit
 exit
+
+# Save config to configuration files
 write integrated
 EOF
